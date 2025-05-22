fc7b33368347b08f89d21f52ec56f93e099a3fe8
mahfujalarony <149530147+mahfujalarony@users.noreply.github.com>
2025-05-22 13:41:18 +0600

    firebase security setup

--- vite-project/src/Components/auth/firebase.ts
+++ vite-project/src/Components/auth/firebase.ts
@@ -1,18 +1,18 @@
-
+// src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVgQnQkNbN_uaAy1LdMnKPHD2Z1wjQjlo",
-  authDomain: "auth-3b4b0.firebaseapp.com",
-  projectId: "auth-3b4b0",
-  storageBucket: "auth-3b4b0.firebasestorage.app",
-  messagingSenderId: "370859770753",
-  appId: "1:370859770753:web:a5d135a3de95d8e4d5de80",
-  measurementId: "G-DWFWDRYEBK"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
